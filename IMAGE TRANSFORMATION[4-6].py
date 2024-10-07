bc8b8cccb36519174d36133e6220cf81c2bd21f5
Loveboysubi <138970879+Loveboysubi@users.noreply.github.com>
2024-10-07 10:19:54 +0530

    Update IMAGE TRANSFORMATION[4-6].py

--- IMAGE TRANSFORMATION[4-6].py
+++ IMAGE TRANSFORMATION[4-6].py
@@ -12,6 +12,7 @@ rows, cols, dim =
 M_x=np.float
 reflected_img_xaxis=cv2.warpPerspective(input_image,M_x,(cols,rows))
 reflected_img_yaxis=cv2.warpPerspective(input_image,M_y,(cols,rows))
+reflected_image = cv2.flip(image_rgb, 1)  # Horizontal reflection (flip along y-axis)
 plt.imshow(reflected_)
 plt.show()
 
@@ -22,6 +23,8 @@ import matplotlib.pyplot as plt
 input_image=cv2.imread("got.jpg") 
 input_image=cv2.cvtColor(input_image, cv2.COLOR_BGR2RGB)
 angle=np.radians(45)
+M_rotate = cv2.getRotationMatrix2D((cols / 2, rows / 2), 45, 1)  # Rotate by 45 degrees
+rotated_image = cv2.warpAffine(image_rgb, M_rotate, (cols, rows))
 M=np.float32([[np.cos(angle),-(np.sin(angle)),0],
                [np.
 rotated_img=
@@ -33,5 +36,6 @@ import cv2
 import matplotlib.pyplot as plt
 input_image=cv2.imread("got.jpg") 
 input_image=cv2.cvtColor(input_image,
+cropped_image = image_rgb[50:300, 100:400]  # Crop a portion of the image                         
 plt.show()
 

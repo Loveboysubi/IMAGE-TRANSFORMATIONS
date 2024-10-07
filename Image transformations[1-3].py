bbb791cf4c8b842e28b250ec767864c3eb9410e2
Loveboysubi <138970879+Loveboysubi@users.noreply.github.com>
2024-10-07 10:18:52 +0530

    Update Image transformations[1-3].py

--- Image transformations[1-3].py
+++ Image transformations[1-3].py
@@ -1,3 +1,6 @@
+# Load the image
+image = cv2.imread('dog.jpg')
+image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB for Matplotlib
 
 ##i)Image Translation
 import numpy as np
@@ -14,7 +17,9 @@ M=np.float32([[1,0,20],
              [0,0,1]])
 translated_img=cv2.
 plt.axis('off')
-
+rows, cols, _ = image.shape
+M_translate = np.float32([[1, 0, 50], [0, 1, 100]])  # Translate by (50, 100) pixels
+translated_image = cv2.warpAffine(image_rgb, M_translate, (cols, rows))
 
 
 ##ii)Image Scaling
@@ -23,6 +28,7 @@ scaled_img=cv2.warpPerspective(input_img,M,(cols,rows))
 plt.axis('off')
 plt.imshow(scaled_img)
 plt.show()
+scaled_image = cv2.resize(image_rgb, None, fx=1.5, fy=1.5, interpolation=cv2.INTER_LINEAR)  # Scale by 1.5x
 
 
 ##iii)Image Shearing
@@ -37,7 +43,8 @@ plt.imshow(sheared_img_xaxis)
 plt.show()
 plt.axis
 plt.show()
-
+M_shear = np.float32([[1, 0.5, 0], [0.5, 1, 0]])  # Shear with factor 0.5
+sheared_image = cv2.warpAffine(image_rgb, M_shear, (int(cols * 1.5), int(rows * 1.5)))
 
 
 
